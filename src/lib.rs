9d0d17023083c469b7d323d7c00d4cba631b6781
Tamir Duberstein <tamird@gmail.com>
2015-03-15 09:57:27 -0700

    Update to Rust master

--- src/lib.rs
+++ src/lib.rs
@@ -224,7 +224,7 @@
 #![doc(html_logo_url = "http://www.rust-lang.org/logos/rust-logo-128x128-blk.png",
        html_favicon_url = "http://www.rust-lang.org/favicon.ico",
        html_root_url = "http://doc.rust-lang.org/rand/")]
-#![feature(core, os, io)]
+#![feature(core, io)]
 
 #![cfg_attr(test, feature(test))]
 #![cfg_attr(test, deny(warnings))]
