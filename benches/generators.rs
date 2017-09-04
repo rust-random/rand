04e326d9cd93ca6a34a5e74a30b71d3338865dc7
Diggory Hardy <git@dhardy.name>
2017-09-04 15:27:24 +0100

    Include gen_bytes_* generator benchmarks; use macros

--- benches/generators.rs
+++ benches/generators.rs
@@ -4,75 +4,56 @@ extern crate test;
 extern crate rand;
 
 const RAND_BENCH_N: u64 = 1000;
+const BYTES_LEN: usize = 1024;
 
 use std::mem::size_of;
 use test::{black_box, Bencher};
 
-use rand::{StdRng, OsRng, Rand, Default};
+use rand::{Rng, StdRng, OsRng, Rand, Default};
 use rand::prng::{XorShiftRng, IsaacRng, Isaac64Rng, ChaChaRng};
 
-#[bench]
-fn gen_usize_xorshift(b: &mut Bencher) {
-    let mut rng = XorShiftRng::new().unwrap();
-    b.iter(|| {
-        for _ in 0..RAND_BENCH_N {
-            black_box(usize::rand(&mut rng, Default));
+macro_rules! gen_bytes {
+    ($fnn:ident, $gen:ident) => {
+        #[bench]
+        fn $fnn(b: &mut Bencher) {
+            let mut rng = $gen::new().unwrap();
+            let mut buf = [0u8; BYTES_LEN];
+            b.iter(|| {
+                for _ in 0..RAND_BENCH_N {
+                    rng.fill_bytes(&mut buf);
+                }
+            });
+            b.bytes = BYTES_LEN as u64 * RAND_BENCH_N;
         }
-    });
-    b.bytes = size_of::<usize>() as u64 * RAND_BENCH_N;
+    }
 }
 
-#[bench]
-fn gen_usize_isaac(b: &mut Bencher) {
-    let mut rng = IsaacRng::new().unwrap();
-    b.iter(|| {
-        for _ in 0..RAND_BENCH_N {
-            black_box(usize::rand(&mut rng, Default));
+gen_bytes!(gen_bytes_xorshift, XorShiftRng);
+gen_bytes!(gen_bytes_isaac, IsaacRng);
+gen_bytes!(gen_bytes_isaac64, Isaac64Rng);
+gen_bytes!(gen_bytes_chacha, ChaChaRng);
+gen_bytes!(gen_bytes_std, StdRng);
+gen_bytes!(gen_bytes_os, OsRng);
+
+
+macro_rules! gen_usize {
+    ($fnn:ident, $gen:ident) => {
+        #[bench]
+        fn $fnn(b: &mut Bencher) {
+            let mut rng = $gen::new().unwrap();
+            b.iter(|| {
+                for _ in 0..RAND_BENCH_N {
+                    black_box(usize::rand(&mut rng, Default));
+                }
+            });
+            b.bytes = size_of::<usize>() as u64 * RAND_BENCH_N;
         }
-    });
-    b.bytes = size_of::<usize>() as u64 * RAND_BENCH_N;
+    }
 }
 
-#[bench]
-fn gen_usize_isaac64(b: &mut Bencher) {
-    let mut rng = Isaac64Rng::new().unwrap();
-    b.iter(|| {
-        for _ in 0..RAND_BENCH_N {
-            black_box(usize::rand(&mut rng, Default));
-        }
-    });
-    b.bytes = size_of::<usize>() as u64 * RAND_BENCH_N;
-}
-
-#[bench]
-fn gen_usize_chacha(b: &mut Bencher) {
-    let mut rng = ChaChaRng::new().unwrap();
-    b.iter(|| {
-        for _ in 0..RAND_BENCH_N {
-            black_box(usize::rand(&mut rng, Default));
-        }
-    });
-    b.bytes = size_of::<usize>() as u64 * RAND_BENCH_N;
-}
-
-#[bench]
-fn gen_usize_std(b: &mut Bencher) {
-    let mut rng = StdRng::new().unwrap();
-    b.iter(|| {
-        for _ in 0..RAND_BENCH_N {
-            black_box(usize::rand(&mut rng, Default));
-        }
-    });
-    b.bytes = size_of::<usize>() as u64 * RAND_BENCH_N;
-}
-
-#[bench]
-fn gen_usize_os(b: &mut Bencher) {
-    let mut rng = OsRng::new().unwrap();
-    b.iter(|| {
-        for _ in 0..RAND_BENCH_N {
-            black_box(usize::rand(&mut rng, Default));
-        }
-    });
-    b.bytes = size_of::<usize>() as u64 * RAND_BENCH_N;
-}
+gen_usize!(gen_usize_xorshift, XorShiftRng);
+gen_usize!(gen_usize_isaac, IsaacRng);
+gen_usize!(gen_usize_isaac64, Isaac64Rng);
+gen_usize!(gen_usize_chacha, ChaChaRng);
+gen_usize!(gen_usize_std, StdRng);
+gen_usize!(gen_usize_os, OsRng);
