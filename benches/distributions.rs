e177594af6f46b9d42c2683b76c6bfde77e94685
Diggory Hardy <git@dhardy.name>
2017-09-04 15:52:52 +0100

    Use macro in distributions bench to reduce source size

--- benches/distributions.rs
+++ benches/distributions.rs
@@ -29,7 +29,6 @@ fn distr_baseline(b: &mut Bencher) {
     b.bytes = size_of::<u64>() as u64 * ::RAND_BENCH_N;
 }
 
-
 #[bench]
 fn distr_range_int(b: &mut Bencher) {
     let mut rng = XorShiftRng::new().unwrap();
@@ -44,20 +43,6 @@ fn distr_range_int(b: &mut Bencher) {
 }
 
 #[bench]
-fn distr_range_float(b: &mut Bencher) {
-    let mut rng = XorShiftRng::new().unwrap();
-    let distr = Range::new(2.26f64, 2.319f64);
-
-    b.iter(|| {
-        for _ in 0..::RAND_BENCH_N {
-            distr.sample(&mut rng);
-        }
-    });
-    b.bytes = size_of::<f64>() as u64 * ::RAND_BENCH_N;
-}
-
-
-#[bench]
 fn distr_range2_int(b: &mut Bencher) {
     let mut rng = XorShiftRng::new().unwrap();
     let distr = range2::Range::new(3i64, 134217671i64);
@@ -70,83 +55,27 @@ fn distr_range2_int(b: &mut Bencher) {
     b.bytes = size_of::<i64>() as u64 * ::RAND_BENCH_N;
 }
 
-#[bench]
-fn distr_range2_float(b: &mut Bencher) {
-    let mut rng = XorShiftRng::new().unwrap();
-    let distr = range2::Range::new(2.26f64, 2.319f64);
+macro_rules! distr_float {
+    ($fnn:ident, $distr:expr) => {
+        #[bench]
+        fn $fnn(b: &mut Bencher) {
+            let mut rng = XorShiftRng::new().unwrap();
+            let distr = $distr;
 
-    b.iter(|| {
-        for _ in 0..::RAND_BENCH_N {
-            distr.sample(&mut rng);
+            b.iter(|| {
+                for _ in 0..::RAND_BENCH_N {
+                    distr.sample(&mut rng);
+                }
+            });
+            b.bytes = size_of::<f64>() as u64 * ::RAND_BENCH_N;
         }
-    });
-    b.bytes = size_of::<f64>() as u64 * ::RAND_BENCH_N;
+    }
 }
 
-
-#[bench]
-fn distr_exp(b: &mut Bencher) {
-    let mut rng = XorShiftRng::new().unwrap();
-    let distr = Exp::new(2.71828 * 3.14159);
-
-    b.iter(|| {
-        for _ in 0..::RAND_BENCH_N {
-            distr.sample(&mut rng);
-        }
-    });
-    b.bytes = size_of::<f64>() as u64 * ::RAND_BENCH_N;
-}
-
-
-#[bench]
-fn distr_normal(b: &mut Bencher) {
-    let mut rng = XorShiftRng::new().unwrap();
-    let distr = Normal::new(-2.71828, 3.14159);
-
-    b.iter(|| {
-        for _ in 0..::RAND_BENCH_N {
-            distr.sample(&mut rng);
-        }
-    });
-    b.bytes = size_of::<f64>() as u64 * ::RAND_BENCH_N;
-}
-
-#[bench]
-fn distr_log_normal(b: &mut Bencher) {
-    let mut rng = XorShiftRng::new().unwrap();
-    let distr = LogNormal::new(-2.71828, 3.14159);
-
-    b.iter(|| {
-        for _ in 0..::RAND_BENCH_N {
-            distr.sample(&mut rng);
-        }
-    });
-    b.bytes = size_of::<f64>() as u64 * ::RAND_BENCH_N;
-}
-
-
-#[bench]
-fn distr_gamma_large_shape(b: &mut Bencher) {
-    let mut rng = XorShiftRng::new().unwrap();
-    let distr = Gamma::new(10., 1.0);
-
-    b.iter(|| {
-        for _ in 0..::RAND_BENCH_N {
-            distr.sample(&mut rng);
-        }
-    });
-    b.bytes = size_of::<f64>() as u64 * ::RAND_BENCH_N;
-}
-
-#[bench]
-fn distr_gamma_small_shape(b: &mut Bencher) {
-    let mut rng = XorShiftRng::new().unwrap();
-    let distr = Gamma::new(0.1, 1.0);
-
-    b.iter(|| {
-        for _ in 0..::RAND_BENCH_N {
-            distr.sample(&mut rng);
-        }
-    });
-    b.bytes = size_of::<f64>() as u64 * ::RAND_BENCH_N;
-}
+distr_float!(distr_range_float, Range::new(2.26f64, 2.319f64));
+distr_float!(distr_range2_float, range2::Range::new(2.26f64, 2.319f64));
+distr_float!(distr_exp, Exp::new(2.71828 * 3.14159));
+distr_float!(distr_normal, Normal::new(-2.71828, 3.14159));
+distr_float!(distr_log_normal, LogNormal::new(-2.71828, 3.14159));
+distr_float!(distr_gamma_large_shape, Gamma::new(10., 1.0));
+distr_float!(distr_gamma_small_shape, Gamma::new(0.1, 1.0));
