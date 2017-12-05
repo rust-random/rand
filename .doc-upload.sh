4ffd71d17a7ae7c18342e4d5ec19780bfdb0dfd3
Diggory Hardy <git@dhardy.name>
2017-12-05 10:34:08 +0000

    Fix doc-upload again: ignore exit status from travis-cargo

--- .doc-upload.sh
+++ .doc-upload.sh
@@ -1,2 +1,5 @@
 pip install 'travis-cargo<0.2' --user && export PATH=$HOME/.local/bin:$PATH
 travis-cargo --only nightly doc-upload
+# Travis cares about exit status. travis-cargo apparently has non-zero exit status when already
+# up to date, so we must ignore that.
+true
