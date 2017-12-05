dfee0f28868286eeba10c3a95e26f35c497282ec
Diggory Hardy <git@dhardy.name>
2017-12-05 12:36:50 +0000

    Misc bench: better test new 'seq' code

--- benches/misc.rs
+++ benches/misc.rs
@@ -5,8 +5,8 @@ extern crate rand;
 
 use test::{black_box, Bencher};
 
-use rand::Rng;
-use rand::{sample, weak_rng};
+use rand::{Rng, weak_rng};
+use rand::seq::*;
 
 #[bench]
 fn misc_shuffle_100(b: &mut Bencher) {
@@ -19,10 +19,44 @@ fn misc_shuffle_100(b: &mut Bencher) {
 }
 
 #[bench]
-fn misc_sample_10_of_100(b: &mut Bencher) {
+fn misc_sample_iter_10_of_100(b: &mut Bencher) {
     let mut rng = weak_rng();
     let x : &[usize] = &[1; 100];
     b.iter(|| {
-        black_box(sample(&mut rng, x, 10));
+        black_box(sample_iter(&mut rng, x, 10).unwrap_or_else(|e| e));
     })
 }
+
+#[bench]
+fn misc_sample_slice_10_of_100(b: &mut Bencher) {
+    let mut rng = weak_rng();
+    let x : &[usize] = &[1; 100];
+    b.iter(|| {
+        black_box(sample_slice(&mut rng, x, 10));
+    })
+}
+
+#[bench]
+fn misc_sample_slice_ref_10_of_100(b: &mut Bencher) {
+    let mut rng = weak_rng();
+    let x : &[usize] = &[1; 100];
+    b.iter(|| {
+        black_box(sample_slice_ref(&mut rng, x, 10));
+    })
+}
+
+macro_rules! sample_indices {
+    ($name:ident, $amount:expr, $length:expr) => {
+        #[bench]
+        fn $name(b: &mut Bencher) {
+            let mut rng = weak_rng();
+            b.iter(|| {
+                black_box(sample_indices(&mut rng, $length, $amount));
+            })
+        }
+    }
+}
+
+sample_indices!(misc_sample_indices_10_of_1k, 10, 1000);
+sample_indices!(misc_sample_indices_50_of_1k, 50, 1000);
+sample_indices!(misc_sample_indices_100_of_1k, 100, 1000);
