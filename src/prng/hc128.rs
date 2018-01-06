7efa831453a8cbe6f56374668634c8921769c2cb
Paul Dicker <pitdicker@gmail.com>
2018-01-06 15:33:13 +0100

    Small improvements to HC-128 documentation

--- src/prng/hc128.rs
+++ src/prng/hc128.rs
@@ -1,10 +1,10 @@
 // Copyright 2017 The Rust Project Developers. See the COPYRIGHT
 // file at the top-level directory of this distribution and at
-// http://rust-lang.org/COPYRIGHT.
+// https://www.rust-lang.org/COPYRIGHT.
 //
 // Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
-// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
-// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
@@ -19,13 +19,13 @@ use impls;
 /// A cryptographically secure random number generator that uses the HC-128
 /// algorithm.
 ///
-/// HC-128 is a stream cipher designed by Hongjun Wu[1], that we use as an RNG.
+/// HC-128 is a stream cipher designed by Hongjun Wu [1], that we use as an RNG.
 /// It is selected as one of the "stream ciphers suitable for widespread
-/// adoption" by eSTREAM[2].
+/// adoption" by eSTREAM [2].
 ///
 /// HC-128 is an array based RNG. In this it is similar to RC-4 and ISAAC before
 /// it, but those have never been proven cryptographically secure (or have even
-/// been broken).
+/// been significantly compromised, as in the case of RC-4 [5]).
 ///
 /// Because HC-128 works with simple indexing into a large array and with a few
 /// operations that parallelize well, it has very good performance. The size of
@@ -33,7 +33,7 @@ use impls;
 ///
 /// This implementation is not based on the version of HC-128 submitted to the
 /// eSTREAM contest, but on a later version by the author with a few small
-/// improvements from December 15, 2009[3].
+/// improvements from December 15, 2009 [3].
 ///
 /// HC-128 has no known weaknesses that are easier to exploit than doing a
 /// brute-force search of 2<sup>128</sup>. A very comprehensive analysis of the
@@ -48,11 +48,15 @@ use impls;
 ///      (http://www.ecrypt.eu.org/stream/)
 ///
 /// [3]: Hongjun Wu, [Stream Ciphers HC-128 and HC-256]
-///      (http://www3.ntu.edu.sg/home/wuhj/research/hc/index.html)
+///      (https://www.ntu.edu.sg/home/wuhj/research/hc/index.html)
 ///
 /// [4]: Shashwat Raizada (January 2015),
 ///      ["Some Results On Analysis And Implementation Of HC-128 Stream Cipher"]
 ///      (http://library.isical.ac.in:8080/jspui/bitstream/123456789/6636/1/TH431.pdf).
+///
+/// [5]: Internet Engineering Task Force (Februari 2015),
+///      ["Prohibiting RC4 Cipher Suites"]
+///      (https://tools.ietf.org/html/rfc7465).
 #[derive(Clone)]
 pub struct Hc128Rng {
     state: Hc128,
