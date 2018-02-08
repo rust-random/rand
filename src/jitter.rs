8e3134ed61277ee5697683c6d318c38b38702260
Diggory Hardy <git@dhardy.name>
2018-02-08 11:58:54 +0000

    Error handling revisions
    New Unexpected error kind
    Error's kind and msg fields are now public
    Some error handling simplifications and revisions

--- src/jitter.rs
+++ src/jitter.rs
@@ -145,6 +145,8 @@ impl ::std::error::Error for TimerError {
 
 impl From<TimerError> for Error {
     fn from(err: TimerError) -> Error {
+        // Timer check is already quite permissive of failures so we don't
+        // expect false-positive failures, i.e. any error is irrecoverable.
         Error::with_cause(ErrorKind::Unavailable,
                               "timer jitter failed basic quality tests", err)
     }
