138006c2f71ccdb040bbc7c891536911d0df4b08
Diggory Hardy <git@dhardy.name>
2018-02-26 17:38:15 +0000

    Add Travis test target: MIPS64
    This a test adding partial Trust tests as in dhardy/master branch

---
+++ utils/ci/script.sh
@@ -0,0 +1,28 @@
+# Derived from https://github.com/japaric/trust
+
+set -ex
+
+main() {
+    if [ ! -z $DISABLE_TESTS ]; then
+        if [ ! -z $DISABLE_STD ]; then
+            cross build --all --no-default-features --target $TARGET
+        else
+            cross build --all --features log,serde-1 --target $TARGET
+        fi
+        return
+    fi
+
+    if [ ! -z $NIGHTLY ]; then
+        cross test --all --tests --no-default-features --features alloc --target $TARGET
+        cross test --all --features serde-1,log,nightly --target $TARGET
+        cross test --all --benches --target $TARGET
+    else
+        cross test --all --tests --no-default-features --target $TARGET
+        cross test --all --features serde-1,log --target $TARGET
+    fi
+}
+
+# we don't run the "test phase" when doing deploys
+if [ -z $TRAVIS_TAG ]; then
+    main
+fi
