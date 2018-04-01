6272ce85bea9e6ecde632cd346898bf54c610039
Paul Dicker <pitdicker@gmail.com>
2018-04-01 19:21:05 +0200

    Try to run only os_rng test (faster)

--- utils/ci/script.sh
+++ utils/ci/script.sh
@@ -4,21 +4,11 @@ set -ex
 
 main() {
     if [ ! -z $DISABLE_TESTS ]; then
-        cross build --all --no-default-features --target $TARGET --release
-        if [ -z $DISABLE_STD ]; then
-            cross build --features log,serde-1 --target $TARGET
-        fi
+        cross build --all --target $TARGET --release
         return
     fi
 
-    if [ ! -z $NIGHTLY ]; then
-        cross test --all --tests --no-default-features --features alloc --target $TARGET
-        cross test --features serde-1,log,nightly --target $TARGET
-        cross test --all --benches --target $TARGET
-    else
-        cross test --all --tests --no-default-features --target $TARGET
-        cross test --features serde-1,log --target $TARGET
-    fi
+    cross test os_rng --target $TARGET
 }
 
 # we don't run the "test phase" when doing deploys
