f792e480ab3720d5b156910716c0aceb360c0b45
Paul Dicker <pitdicker@gmail.com>
2018-04-07 17:08:47 +0200

    Add simd_support feature

--- src/lib.rs
+++ src/lib.rs
@@ -233,6 +233,7 @@
 #![cfg_attr(all(feature="alloc", not(feature="std")), feature(alloc))]
 #![cfg_attr(all(feature="i128_support", feature="nightly"), allow(stable_features))] // stable since 2018-03-27
 #![cfg_attr(all(feature="i128_support", feature="nightly"), feature(i128_type, i128))]
+#![cfg_attr(all(feature="simd_support", feature="nightly"), feature(stdsimd))]
 #![cfg_attr(feature = "stdweb", recursion_limit="128")]
 
 #[cfg(feature="std")] extern crate std as core;
