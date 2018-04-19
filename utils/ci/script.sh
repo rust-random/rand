5f888a72ab5a61197aa1ed3a4d22f31aa443d7b2
Diggory Hardy <git@dhardy.name>
2018-04-19 11:44:01 +0100

    Enable std by default in rand_core; adjust CI targets
    Avoid using --all and --features together

--- utils/ci/script.sh
+++ utils/ci/script.sh
@@ -3,20 +3,22 @@
 set -ex
 
 main() {
-    if [ ! -z $DISABLE_TESTS ]; then
-        cross build --all --no-default-features --target $TARGET --release
-        if [ -z $DISABLE_STD ]; then
+    if [ ! -z $DISABLE_TESTS ]; then    # tests are disabled
+        cross build --no-default-features --target $TARGET --release
+        if [ -z $DISABLE_STD ]; then    # std is enabled
             cross build --features log,serde1 --target $TARGET
         fi
         return
     fi
 
-    if [ ! -z $NIGHTLY ]; then
-        cross test --all --tests --no-default-features --features alloc --target $TARGET
-        cross test --features serde1,log,nightly --target $TARGET
+    if [ ! -z $NIGHTLY ]; then  # have nightly Rust
+        cross test --tests --no-default-features --features alloc --target $TARGET
+        cross test --package rand_core --no-default-features --features alloc --target $TARGET
+        cross test --features serde1,log,nightly,alloc --target $TARGET
         cross test --all --benches --target $TARGET
-    else
-        cross test --all --tests --no-default-features --target $TARGET
+    else    # have stable Rust
+        cross test --tests --no-default-features --target $TARGET
+        cross test --package rand_core --no-default-features --target $TARGET
         cross test --features serde1,log --target $TARGET
     fi
 }
