06cd3896032a046d9a3fc968f5bc297606521879
Vinzent Steinberg <Vinzent.Steinberg@gmail.com>
2018-04-24 21:53:59 +0200

    Add copyright notice

--- src/distributions/bernoulli.rs
+++ src/distributions/bernoulli.rs
@@ -1,3 +1,12 @@
+// Copyright 2018 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// https://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
 //! The Bernoulli distribution.
 
 use Rng;
