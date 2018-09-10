92aa49804b63fc71b8f624d30f77c8c49bde57c6
Diggory Hardy <git@dhardy.name>
2018-09-10 15:37:36 +0100

    Remove references to COPYRIGHT file which are not needed
    We have no sub-packages under different licences

--- utils/ziggurat_tables.py
+++ utils/ziggurat_tables.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright 2013 The Rust Project Developers. See the COPYRIGHT
-# file at the top-level directory of this distribution and at
-# https://rust-lang.org/COPYRIGHT.
+# Copyright 2013 The Rust Project Developers.
 #
 # Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
 # https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
@@ -105,9 +103,7 @@ def render_table(name, values):
 
 
 with open('ziggurat_tables.rs', 'w') as f:
-    f.write('''// Copyright 2013 The Rust Project Developers. See the COPYRIGHT
-// file at the top-level directory of this distribution and at
-// https://rust-lang.org/COPYRIGHT.
+    f.write('''// Copyright 2013 The Rust Project Developers.
 //
 // Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
 // https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
