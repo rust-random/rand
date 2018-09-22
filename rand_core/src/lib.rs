7dd05ed335e863d39b944ebc832c27ec2c82cb1c
Diggory Hardy <git@dhardy.name>
2018-09-22 10:21:00 +0100

    Prepare rand_core 0.3 release

--- rand_core/src/lib.rs
+++ rand_core/src/lib.rs
@@ -34,7 +34,7 @@
 
 #![doc(html_logo_url = "https://www.rust-lang.org/logos/rust-logo-128x128-blk.png",
        html_favicon_url = "https://www.rust-lang.org/favicon.ico",
-       html_root_url = "https://docs.rs/rand_core/0.2.1")]
+       html_root_url = "https://docs.rs/rand_core/0.3.0")]
 
 #![deny(missing_docs)]
 #![deny(missing_debug_implementations)]
