905e2552844734cebe8dc0abd29d2037d989655f
Diggory Hardy <git@dhardy.name>
2018-10-15 11:05:15 +0100

    Revise CI scripts

--- utils/ci/script.sh
+++ utils/ci/script.sh
@@ -3,36 +3,17 @@
 set -ex
 
 main() {
-    if [ ! -z $DISABLE_TESTS ]; then    # tests are disabled
-        cross build --no-default-features --target $TARGET --release
-        if [ -z $DISABLE_STD ]; then    # std is enabled
-            cross build --features log,serde1 --target $TARGET
-        fi
-        return
-    fi
-
-    if [ ! -z $NIGHTLY ]; then  # have nightly Rust
-        cross test --lib --no-default-features --features alloc --target $TARGET
-        cross test --all-features --target $TARGET
-        cross test --benches --features=nightly --target $TARGET
-        cross test --examples --target $TARGET
-        cross test --package rand_core --target $TARGET
-        cross test --package rand_core --no-default-features --features=alloc --target $TARGET
-        cross test --package rand_isaac --features=serde1 --target $TARGET
-        cross test --package rand_chacha --target $TARGET
-        cross test --package rand_hc128 --target $TARGET
-        cross test --package rand_xorshift --features=serde1 --target $TARGET
-    else    # have stable Rust
-        cross test --lib --no-default-features --target $TARGET
-        cross test --features=serde1,log --target $TARGET
-        cross test --examples --target $TARGET
-        cross test --package rand_core --target $TARGET
-        cross test --package rand_core --no-default-features --target $TARGET
-        cross test --package rand_isaac --features=serde1 --target $TARGET
-        cross test --package rand_chacha --target $TARGET
-        # cross test --package rand_hc128 ---target $TARGET  # fails for unknown reasons
-        cross test --package rand_xorshift --features=serde1 --target $TARGET
-    fi
+    cross test --target $TARGET --lib --no-default-features
+  # TODO: add simd_support feature:
+    cross test --target $TARGET --features=serde1,log
+    cross test --target $TARGET --examples
+    cross test --target $TARGET --manifest-path rand_core/Cargo.toml
+    cross test --target $TARGET --manifest-path rand_core/Cargo.toml --no-default-features
+    cross test --target $TARGET --manifest-path rand_isaac/Cargo.toml --features=serde1
+    cross test --target $TARGET --manifest-path rand_pcg/Cargo.toml --features=serde1
+    cross test --target $TARGET --manifest-path rand_xorshift/Cargo.toml --features=serde1
+    cross test --target $TARGET --manifest-path rand_chacha/Cargo.toml
+    cross test --target $TARGET --manifest-path rand_hc128/Cargo.toml
 }
 
 # we don't run the "test phase" when doing deploys
