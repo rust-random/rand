a52d8c1fdf09d87f8cebea0a5def8aa262c1708f
Vinzent Steinberg <Vinzent.Steinberg@gmail.com>
2018-11-14 12:35:32 +0100

    Import xoshiro crate

---
+++ rand_xoshiro/src/xoroshiro64starstar.rs
@@ -0,0 +1,90 @@
+use byteorder::{ByteOrder, LittleEndian};
+use rand_core;
+use rand_core::le::read_u32_into;
+use rand_core::impls::{fill_bytes_via_next, next_u64_via_u32};
+use rand_core::{RngCore, SeedableRng};
+
+/// A Xoroshiro64** random number generator.
+///
+/// The xoshiro64** algorithm is not suitable for cryptographic purposes, but
+/// is very fast and has excellent statistical properties.
+///
+/// The algorithm used here is translated from [the `xoroshiro64starstar.c`
+/// reference source code](http://xoshiro.di.unimi.it/xoroshiro64starstar.c) by
+/// David Blackman and Sebastiano Vigna.
+#[allow(missing_copy_implementations)]
+#[derive(Debug, Clone)]
+pub struct Xoroshiro64StarStar {
+    s0: u32,
+    s1: u32,
+}
+
+impl Xoroshiro64StarStar {
+    /// Seed a `Xoroshiro64StarStar` from a `u64`.
+    pub fn from_seed_u64(seed: u64) -> Xoroshiro64StarStar {
+        let mut s = [0; 8];
+        LittleEndian::write_u64(&mut s, seed);
+        Xoroshiro64StarStar::from_seed(s)
+    }
+}
+
+impl RngCore for Xoroshiro64StarStar {
+    #[inline]
+    fn next_u32(&mut self) -> u32 {
+        let r = starstar_u32!(self.s0);
+        impl_xoroshiro_u32!(self);
+        r
+    }
+
+    #[inline]
+    fn next_u64(&mut self) -> u64 {
+        next_u64_via_u32(self)
+    }
+
+    #[inline]
+    fn fill_bytes(&mut self, dest: &mut [u8]) {
+        fill_bytes_via_next(self, dest);
+    }
+
+    #[inline]
+    fn try_fill_bytes(&mut self, dest: &mut [u8]) -> Result<(), rand_core::Error> {
+        self.fill_bytes(dest);
+        Ok(())
+    }
+}
+
+impl SeedableRng for Xoroshiro64StarStar {
+    type Seed = [u8; 8];
+
+    /// Create a new `Xoroshiro64StarStar`.  If `seed` is entirely 0, it will be
+    /// mapped to a different seed.
+    fn from_seed(seed: [u8; 8]) -> Xoroshiro64StarStar {
+        deal_with_zero_seed!(seed, Self);
+        let mut s = [0; 2];
+        read_u32_into(&seed, &mut s);
+
+        Xoroshiro64StarStar {
+            s0: s[0],
+            s1: s[1],
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn reference() {
+        let mut rng = Xoroshiro64StarStar::from_seed([1, 0, 0, 0, 2, 0, 0, 0]);
+        // These values were produced with the reference implementation:
+        // http://xoshiro.di.unimi.it/xoshiro64starstar.c
+        let expected = [
+            3802928447, 813792938, 1618621494, 2955957307, 3252880261,
+            1129983909, 2539651700, 1327610908, 1757650787, 2763843748,
+        ];
+        for &e in &expected {
+            assert_eq!(rng.next_u32(), e);
+        }
+    }
+}
---
+++ rand_xoshiro/src/xoshiro128starstar.rs
@@ -0,0 +1,105 @@
+use rand_core::impls::{next_u64_via_u32, fill_bytes_via_next};
+use rand_core::le::read_u32_into;
+use rand_core::{SeedableRng, RngCore, Error};
+
+/// A xoshiro128** random number generator.
+///
+/// The xoshiro128** algorithm is not suitable for cryptographic purposes, but
+/// is very fast and has excellent statistical properties.
+///
+/// The algorithm used here is translated from [the `xoshiro128starstar.c`
+/// reference source code](http://xoshiro.di.unimi.it/xoshiro128starstar.c) by
+/// David Blackman and Sebastiano Vigna.
+#[derive(Debug, Clone)]
+pub struct Xoshiro128StarStar {
+    s: [u32; 4],
+}
+
+impl Xoshiro128StarStar {
+    /// Seed a `Xoshiro128StarStar` from a `u64` using `SplitMix64`.
+    pub fn from_seed_u64(seed: u64) -> Xoshiro128StarStar {
+        from_splitmix!(seed)
+    }
+
+    /// Jump forward, equivalently to 2^64 calls to `next_u32()`.
+    ///
+    /// This can be used to generate 2^64 non-overlapping subsequences for
+    /// parallel computations.
+    ///
+    /// ```
+    /// # extern crate rand;
+    /// # extern crate rand_xoshiro;
+    /// # fn main() {
+    /// use rand::SeedableRng;
+    /// use rand_xoshiro::Xoroshiro128StarStar;
+    ///
+    /// let rng1 = Xoroshiro128StarStar::from_seed_u64(0);
+    /// let mut rng2 = rng1.clone();
+    /// rng2.jump();
+    /// let mut rng3 = rng2.clone();
+    /// rng3.jump();
+    /// # }
+    /// ```
+    pub fn jump(&mut self) {
+        impl_jump!(u32, self, [0x8764000b, 0xf542d2d3, 0x6fa035c3, 0x77f2db5b]);
+    }
+}
+
+impl SeedableRng for Xoshiro128StarStar {
+    type Seed = [u8; 16];
+
+    /// Create a new `Xoshiro128StarStar`.  If `seed` is entirely 0, it will be
+    /// mapped to a different seed.
+    #[inline]
+    fn from_seed(seed: [u8; 16]) -> Xoshiro128StarStar {
+        deal_with_zero_seed!(seed, Self);
+        let mut state = [0; 4];
+        read_u32_into(&seed, &mut state);
+        Xoshiro128StarStar { s: state }
+    }
+}
+
+impl RngCore for Xoshiro128StarStar {
+    #[inline]
+    fn next_u32(&mut self) -> u32 {
+        let result_starstar = starstar_u64!(self.s[0]);
+        impl_xoshiro_u32!(self);
+        result_starstar
+    }
+
+    #[inline]
+    fn next_u64(&mut self) -> u64 {
+        next_u64_via_u32(self)
+    }
+
+    #[inline]
+    fn fill_bytes(&mut self, dest: &mut [u8]) {
+        fill_bytes_via_next(self, dest);
+    }
+
+    #[inline]
+    fn try_fill_bytes(&mut self, dest: &mut [u8]) -> Result<(), Error> {
+        self.fill_bytes(dest);
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn reference() {
+        let mut rng = Xoshiro128StarStar::from_seed(
+            [1, 0, 0, 0, 2, 0, 0, 0, 3, 0, 0, 0, 4, 0, 0, 0]);
+        // These values were produced with the reference implementation:
+        // http://xoshiro.di.unimi.it/xoshiro128starstar.c
+        let expected = [
+            5760, 40320, 70819200, 3297914139, 2480851620, 1792823698,
+            4118739149, 1251203317, 1581886583, 1721184582,
+        ];
+        for &e in &expected {
+            assert_eq!(rng.next_u32(), e);
+        }
+    }
+}
