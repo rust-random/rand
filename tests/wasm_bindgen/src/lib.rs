23c47fdcd6ef904644882e3d3778fcbc861c7dd1
Stephen Sherratt <sfsherratt@gmail.com>
2019-01-12 14:07:40 +0000

    Add minimal wasm-bindgen test crate

---
+++ tests/wasm_bindgen/src/lib.rs
@@ -0,0 +1,23 @@
+extern crate rand;
+extern crate wasm_bindgen;
+extern crate wasm_bindgen_test;
+
+use rand::rngs::StdRng;
+use rand::Rng;
+use rand::SeedableRng;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen_test::*;
+
+#[wasm_bindgen]
+pub fn generate_from_seed(seed: u64) -> u64 {
+    StdRng::seed_from_u64(seed).gen()
+}
+
+pub mod tests {
+    use super::*;
+
+    #[wasm_bindgen_test]
+    fn generate_from_seed_test() {
+        let _ = generate_from_seed(42);
+    }
+}
