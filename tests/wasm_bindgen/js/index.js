6775296568a4c5e6708be94ca374329d096b2cc2
Stephen Sherratt <sfsherratt@gmail.com>
2019-01-16 21:34:13 +0000

    Add semicolons to js file in wasm bindgen test

--- tests/wasm_bindgen/js/index.js
+++ tests/wasm_bindgen/js/index.js
@@ -2,6 +2,6 @@
 
 const rand_wasm_bindgen_test = require('./rand_wasm_bindgen_test');
 
-console.log(rand_wasm_bindgen_test.generate_from_entropy())
-console.log(rand_wasm_bindgen_test.generate_from_os_rand())
-console.log(rand_wasm_bindgen_test.generate_from_seed())
+console.log(rand_wasm_bindgen_test.generate_from_entropy());
+console.log(rand_wasm_bindgen_test.generate_from_os_rand());
+console.log(rand_wasm_bindgen_test.generate_from_seed());
