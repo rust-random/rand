9a72dd9d90a26f706ff9fe206e74853ef9165ed4
Stephen Sherratt <sfsherratt@gmail.com>
2019-01-16 21:09:28 +0000

    Add test of entropy and os to wasm_bindgen test

--- tests/wasm_bindgen/src/lib.rs
+++ tests/wasm_bindgen/src/lib.rs
@@ -2,14 +2,24 @@ extern crate rand;
 extern crate wasm_bindgen;
 extern crate wasm_bindgen_test;
 
-use rand::rngs::StdRng;
-use rand::Rng;
-use rand::SeedableRng;
+use rand::rngs::{OsRng, StdRng};
+use rand::FromEntropy;
+use rand::{Rng, RngCore, SeedableRng};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn generate_from_seed(seed: u32) -> u32 {
+pub fn generate_from_seed(seed: u32) -> i32 {
     StdRng::seed_from_u64(seed as u64).gen()
+}
+
+#[wasm_bindgen]
+pub fn generate_from_os_rand() -> i32 {
+    OsRng::new().unwrap().gen()
+}
+
+#[wasm_bindgen]
+pub fn generate_from_entropy() -> i32 {
+    StdRng::from_entropy().gen()
 }
 
 pub mod tests {
