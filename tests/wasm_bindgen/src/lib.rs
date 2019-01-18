13dafd9212a533f852648c51d3c4798e98c7b827
Stephen Sherratt <sfsherratt@gmail.com>
2019-01-18 23:35:27 +0000

    Re-add wasm-bindgen-test tests

--- tests/wasm_bindgen/src/lib.rs
+++ tests/wasm_bindgen/src/lib.rs
@@ -1,5 +1,6 @@
 extern crate rand;
 extern crate wasm_bindgen;
+extern crate wasm_bindgen_test;
 
 use rand::rngs::{OsRng, StdRng};
 use rand::FromEntropy;
@@ -19,4 +20,23 @@ pub fn generate_from_os_rand() -> i32 {
 #[wasm_bindgen]
 pub fn generate_from_entropy() -> i32 {
     StdRng::from_entropy().gen()
+}
+
+pub mod tests {
+    use wasm_bindgen_test::*;
+
+    #[wasm_bindgen_test]
+    fn generate_from_seed() {
+        let _ = super::generate_from_seed(42);
+    }
+
+    #[wasm_bindgen_test]
+    fn generate_from_os_rand() {
+        let _ = super::generate_from_os_rand();
+    }
+
+    #[wasm_bindgen_test]
+    fn generate_from_entropy() {
+        let _ = super::generate_from_entropy();
+    }
 }
