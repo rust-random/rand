f6d740712fe1fc79c8d49cf17030a28ebee2aece
Vinzent Steinberg <Vinzent.Steinberg@gmail.com>
2019-02-21 16:08:15 +0100

    Binomial: Use correct sign for stirling correction
    This was noted by the GSL implementors. The new signs were confirmed by
    one of the authors publishing the original algorithm.

--- src/distributions/binomial.rs
+++ src/distributions/binomial.rs
@@ -233,7 +233,7 @@ impl Distribution<u64> for Binomial {
                 if alpha > x_m * (f1 / x1).ln()
                     + (n - (m as f64) + 0.5) * (z / w).ln()
                     + ((y - m) as f64) * (w * p / (x1 * q)).ln()
-                    + stirling(f1) + stirling(z) + stirling(x1) + stirling(w)
+                    + stirling(f1) + stirling(z) - stirling(x1) - stirling(w)
                 {
                     continue;
                 }
