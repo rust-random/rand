a9a49302f2e87290484363b6b311b0b25dfede87
Kaz Wesley <kaz@lambdaverse.org>
2019-05-03 09:43:55 -0700

    restore copyright notice

--- rand_chacha/src/chacha.rs
+++ rand_chacha/src/chacha.rs
@@ -1,3 +1,13 @@
+// Copyright 2018 Developers of the Rand project.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+//! The ChaCha random number generator.
+
 #[cfg(feature = "std")]
 use std as core;
 #[cfg(not(feature = "std"))]
