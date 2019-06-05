99230cfc0d102d0fefd6c7e3503798c2224c0613
Diggory Hardy <git@dhardy.name>
2019-06-05 10:40:57 +0100

    Update XorShiftRng::from_rng test
    We now have value stability, so should test this

--- rand_xorshift/tests/mod.rs
+++ rand_xorshift/tests/mod.rs
@@ -12,9 +12,10 @@ fn test_xorshift_construction() {
     let mut rng1 = XorShiftRng::from_seed(seed);
     assert_eq!(rng1.next_u64(), 4325440999699518727);
 
-    let _rng2 = XorShiftRng::from_rng(rng1).unwrap();
-    // Note: we cannot test the state of _rng2 because from_rng does not
-    // fix Endianness. This is allowed in the trait specification.
+    let mut rng2 = XorShiftRng::from_rng(&mut rng1).unwrap();
+    // Yes, this makes rng2 a clone of rng1!
+    assert_eq!(rng1.next_u64(), 15614385950550801700);
+    assert_eq!(rng2.next_u64(), 15614385950550801700);
 }
 
 #[test]
