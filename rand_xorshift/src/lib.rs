17986eb652deb6732cb9e2b27d6b7aa4d5b38173
Diggory Hardy <git@dhardy.name>
2019-06-11 11:16:17 +0100

    Prepare rand_xorshift 0.2.0 and update to edition 2018

--- rand_xorshift/src/lib.rs
+++ rand_xorshift/src/lib.rs
@@ -17,14 +17,10 @@
 
 #![no_std]
 
-pub extern crate rand_core;
-
-#[cfg(feature="serde1")] extern crate serde;
-#[cfg(feature="serde1")] #[macro_use] extern crate serde_derive;
-
 use core::num::Wrapping as w;
 use core::{fmt, slice};
 use rand_core::{RngCore, SeedableRng, Error, impls, le};
+#[cfg(feature="serde")] use serde::{Serialize, Deserialize};
 
 /// An Xorshift random number generator.
 ///
@@ -36,7 +32,7 @@ use rand_core::{RngCore, SeedableRng, Error, impls, le};
 ///       ["Xorshift RNGs"](https://www.jstatsoft.org/v08/i14/paper).
 ///       *Journal of Statistical Software*. Vol. 8 (Issue 14).
 #[derive(Clone)]
-#[cfg_attr(feature="serde1", derive(Serialize,Deserialize))]
+#[cfg_attr(feature="serde", derive(Serialize,Deserialize))]
 pub struct XorShiftRng {
     x: w<u32>,
     y: w<u32>,
