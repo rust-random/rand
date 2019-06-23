67f31795d4a2d862b204a4f9cf8e9eb4ea9083a4
Vinzent Steinberg <Vinzent.Steinberg@gmail.com>
2019-06-23 12:25:53 +0200

    rand_jitter: Fix log feature

--- rand_jitter/src/lib.rs
+++ rand_jitter/src/lib.rs
@@ -71,8 +71,37 @@ macro_rules! doc_comment {
 #[cfg(test)]
 doc_comment!(include_str!("../README.md"));
 
-#[cfg(not(feature = "log"))]
-#[macro_use] mod dummy_log;
+#[allow(unused)]
+macro_rules! trace { ($($x:tt)*) => (
+    #[cfg(feature = "log")] {
+        log::trace!($($x)*)
+    }
+) }
+#[allow(unused)]
+macro_rules! debug { ($($x:tt)*) => (
+    #[cfg(feature = "log")] {
+        log::debug!($($x)*)
+    }
+) }
+#[allow(unused)]
+macro_rules! info { ($($x:tt)*) => (
+    #[cfg(feature = "log")] {
+        log::info!($($x)*)
+    }
+) }
+#[allow(unused)]
+macro_rules! warn { ($($x:tt)*) => (
+    #[cfg(feature = "log")] {
+        log::warn!($($x)*)
+    }
+) }
+#[allow(unused)]
+macro_rules! error { ($($x:tt)*) => (
+    #[cfg(feature = "log")] {
+        log::error!($($x)*)
+    }
+) }
+
 #[cfg(feature = "std")]
 mod platform;
 mod error;
