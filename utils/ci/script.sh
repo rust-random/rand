5b530fbb89c705e518f21f9f42336514b63adee3
Vinzent Steinberg <Vinzent.Steinberg@gmail.com>
2019-06-24 17:47:18 +0200

    Change miri and cross platform checks for serde1 feature
    - `rand_xoshiro` should be built with the feature.
    -  For `rand`, it does not do anything. Instead, test it for
       `rand_core`.

--- utils/ci/script.sh
+++ utils/ci/script.sh
@@ -5,15 +5,16 @@ set -ex
 main() {
     cross test --target $TARGET --tests --no-default-features
   # TODO: add simd_support feature:
-    cross test --target $TARGET --features=serde1,log
+    cross test --target $TARGET --features=log
     cross test --target $TARGET --examples
     cross test --target $TARGET --manifest-path rand_core/Cargo.toml
+    cross test --target $TARGET --manifest-path rand_core/Cargo.toml --features=serde1
     cross test --target $TARGET --manifest-path rand_core/Cargo.toml --no-default-features
     cross test --target $TARGET --manifest-path rand_distr/Cargo.toml
     cross test --target $TARGET --manifest-path rand_isaac/Cargo.toml --features=serde1
     cross test --target $TARGET --manifest-path rand_pcg/Cargo.toml --features=serde1
     cross test --target $TARGET --manifest-path rand_xorshift/Cargo.toml --features=serde1
-    cross test --target $TARGET --manifest-path rand_xoshiro/Cargo.toml
+    cross test --target $TARGET --manifest-path rand_xoshiro/Cargo.toml --features=serde1
     cross test --target $TARGET --manifest-path rand_chacha/Cargo.toml
     cross test --target $TARGET --manifest-path rand_hc/Cargo.toml
     cross test --target $TARGET --manifest-path rand_os/Cargo.toml
