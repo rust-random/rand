49da56132b8cc746a16536a0966e27c01c55c147
Diggory Hardy <git@dhardy.name>
2019-08-28 16:05:53 +0100

    Travis CI: update scripts; remove redundencies

--- utils/ci/script.sh
+++ utils/ci/script.sh
@@ -2,23 +2,55 @@
 
 set -ex
 
+# ----- Options -----
+
+# TARGET enables cross-building
+if [ -z $TARGET ]; then
+    CARGO=cargo
+else
+    CARGO="cross --target $TARGET"
+fi
+
+# ALLOC defaults on; is disabled for rustc < 1.36
+if [ -z $ALLOC ]; then
+    ALLOC=1
+fi
+
+# NIGHTLY defaults off
+
+
+# ----- Script -----
+
 main() {
-    cross test --target $TARGET --tests --no-default-features
-  # TODO: add simd_support feature:
-    cross test --target $TARGET --features=log
-    cross test --target $TARGET --examples
-    cross test --target $TARGET --manifest-path rand_core/Cargo.toml
-    cross test --target $TARGET --manifest-path rand_core/Cargo.toml --features=serde1
-    cross test --target $TARGET --manifest-path rand_core/Cargo.toml --no-default-features
-    cross test --target $TARGET --manifest-path rand_distr/Cargo.toml
-    cross test --target $TARGET --manifest-path rand_isaac/Cargo.toml --features=serde1
-    cross test --target $TARGET --manifest-path rand_pcg/Cargo.toml --features=serde1
-    cross test --target $TARGET --manifest-path rand_xorshift/Cargo.toml --features=serde1
-    cross test --target $TARGET --manifest-path rand_xoshiro/Cargo.toml --features=serde1
-    cross test --target $TARGET --manifest-path rand_chacha/Cargo.toml
-    cross test --target $TARGET --manifest-path rand_hc/Cargo.toml
-    cross test --target $TARGET --manifest-path rand_os/Cargo.toml
-    cross test --target $TARGET --manifest-path rand_jitter/Cargo.toml
+  if [ "0$NIGHTLY" -ge 1 ]; then
+    $CARGO test --all-features
+    $CARGO test --benches --features=nightly
+  else
+    # all stable features:
+    $CARGO test --features=serde1,log,small_rng
+  fi
+
+  if [ "$ALLOC" -ge 1 ]; then
+    $CARGO test --tests --no-default-features --features=alloc,getrandom,small_rng
+  fi
+  
+  $CARGO test --tests --no-default-features
+  $CARGO test --examples
+  
+  $CARGO test --manifest-path rand_core/Cargo.toml
+  $CARGO test --manifest-path rand_core/Cargo.toml --no-default-features
+  $CARGO test --manifest-path rand_core/Cargo.toml --no-default-features --features=alloc
+  $CARGO test --manifest-path rand_core/Cargo.toml --no-default-features --features=getrandom
+  
+  $CARGO test --manifest-path rand_distr/Cargo.toml
+  $CARGO test --manifest-path rand_isaac/Cargo.toml --features=serde1
+  $CARGO test --manifest-path rand_pcg/Cargo.toml --features=serde1
+  $CARGO test --manifest-path rand_xorshift/Cargo.toml --features=serde1
+  $CARGO test --manifest-path rand_xoshiro/Cargo.toml
+  $CARGO test --manifest-path rand_chacha/Cargo.toml
+  $CARGO test --manifest-path rand_hc/Cargo.toml
+  $CARGO test --manifest-path rand_jitter/Cargo.toml
+  $CARGO test --manifest-path rand_os/Cargo.toml
 }
 
 # we don't run the "test phase" when doing deploys
