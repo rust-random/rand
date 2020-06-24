5dc66daeffedf7503612a21e92b4bfe16f5ce9f1
Артём Павлов [Artyom Pavlov] <newpavlov@gmail.com>
2020-06-24 15:34:23 +0300

    return new line

--- rand_distr/tests/uniformity.rs
+++ rand_distr/tests/uniformity.rs
@@ -5,6 +5,7 @@
 // <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
+
 use average::Histogram;
 use rand::prelude::*;
 
