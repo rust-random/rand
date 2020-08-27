9577e0b1c941038d60566e1880580a44c4f24639
Vinzent Steinberg <Vinzent.Steinberg@gmail.com>
2020-08-27 17:13:37 +0200

    Slightly improve test

--- src/distributions/mod.rs
+++ src/distributions/mod.rs
@@ -343,7 +343,7 @@ mod tests {
         for _ in 0..100 {
             sum += iter.next().unwrap();
         }
-        assert!(sum > 0.);
+        assert!(0. < sum && sum < 100.);
     }
 
     #[test]
