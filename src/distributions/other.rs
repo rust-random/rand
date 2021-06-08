fa17d1c1a292e6bb95743885c839a8a902e89e81
Diggory Hardy <git@dhardy.name>
2021-06-08 08:16:07 +0100

    Add comment to append_string for Standard

--- src/distributions/other.rs
+++ src/distributions/other.rs
@@ -94,6 +94,9 @@ impl Distribution<char> for Standard {
 #[cfg(feature = "alloc")]
 impl DistString for Standard {
     fn append_string<R: Rng + ?Sized>(&self, rng: &mut R, s: &mut String, len: usize) {
+        // A char is encoded with at most four bytes, thus this reservation is
+        // guaranteed to be sufficient. We do not shrink_to_fit afterwards so
+        // that repeated usage on the same `String` buffer does not reallocate.
         s.reserve(s.len() + 4 * len);
         s.extend(Distribution::<char>::sample_iter(self, rng).take(len));
     }
