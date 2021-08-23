b86ec29224b648f3f55c3325558ebccc9cf81f79
Vinzent Steinberg <Vinzent.Steinberg@gmail.com>
2021-08-23 17:55:18 +0200

    BlockRng::next_u32: Make sure to use half-entry at the previous index

--- rand_core/src/block.rs
+++ rand_core/src/block.rs
@@ -372,12 +372,14 @@ where
 
     #[inline]
     fn next_u64(&mut self) -> u64 {
-        if self.index >= self.results.as_ref().len() {
+        let mut index = self.index - self.half_used as usize;
+        if index >= self.results.as_ref().len() {
             self.core.generate(&mut self.results);
             self.index = 0;
+            index = 0;
         }
 
-        let value = self.results.as_ref()[self.index];
+        let value = self.results.as_ref()[index];
         self.index += 1;
         self.half_used = false;
         value
