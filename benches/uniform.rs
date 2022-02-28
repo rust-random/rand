2c36d18bf395afdf14d465ea864cd4bc91420bbe
Diggory Hardy <git@dhardy.name>
2022-02-28 11:21:50 +0000

    Bench: add documentation and TODO notes; adjust names

--- benches/uniform.rs
+++ benches/uniform.rs
@@ -11,9 +11,10 @@ use criterion::{BenchmarkId, Criterion};
 use rand::distributions::uniform::{SampleUniform, Uniform, UniformSampler};
 use rand::prelude::*;
 
+// TODO: multiple RNGs
 type BenchRng = SmallRng;
 
-macro_rules! bench_dist_int_group {
+macro_rules! bench_distr_int_group {
     ($name:literal, $T:ty, $f:ident, $g:expr, $inputs:expr) => {
         for input in $inputs {
             $g.bench_with_input(
@@ -29,7 +30,7 @@ macro_rules! bench_dist_int_group {
     };
 }
 
-macro_rules! bench_int_group {
+macro_rules! bench_single_int_group {
     ($name:literal, $T:ty, $f:ident, $g:expr, $inputs:expr) => {
         for input in $inputs {
             $g.bench_with_input(
@@ -52,26 +53,43 @@ macro_rules! bench_int_group {
     };
 }
 
+/// Implement benchmarks for integer types
+///
+/// This benchmark consists of two groups:
+///
+/// -   `uniform_distr_int_*`: construct a distribution then sample from it repeatedly
+/// -   `uniform_single_int_*`: use sample_single_inclusive methods, using a different range each time
+///
+/// For each group, we have the following methods:
+///
+/// -   Old: prior implementation of this method as a baseline
+/// -   Lemire: widening multiply with rejection test
+/// -   Canon: widening multiply using max(64, ty-bits) sample with bias reduction adjustment
+/// -   Canon32 (TODO): Canon's method with 32-bit samples (up to two bias reduction steps)
+/// -   Canon64: for 8- and 16-bit types this is just biased widening multiply; for 128-bit
+///     types this is Canon's method with 64-bit sample
+/// -   Canon-Lemire: as Canon but with more precise bias-reduction step trigger
+/// -   Bitmask: bitmasking + rejection method
 macro_rules! bench_int {
     ($c:expr, $T:ty, $high:expr) => {{
         let inputs = &[("high_reject", $high), ("low_reject", (-1, 2))];
 
-        let mut g = $c.benchmark_group(concat!("uniform_dist_int_", stringify!($T)));
-        bench_dist_int_group!("Old", $T, sample, g, inputs);
-        bench_dist_int_group!("Lemire", $T, sample_lemire, g, inputs);
-        bench_dist_int_group!("Canon", $T, sample_canon, g, inputs);
-        bench_dist_int_group!("Canon64", $T, sample_canon_64, g, inputs);
-        bench_dist_int_group!("Canon-Lemire", $T, sample_canon_lemire, g, inputs);
-        bench_dist_int_group!("Bitmask", $T, sample_bitmask, g, inputs);
+        let mut g = $c.benchmark_group(concat!("uniform_distr_int_", stringify!($T)));
+        bench_distr_int_group!("Old", $T, sample, g, inputs);
+        bench_distr_int_group!("Lemire", $T, sample_lemire, g, inputs);
+        bench_distr_int_group!("Canon", $T, sample_canon, g, inputs);
+        bench_distr_int_group!("Canon64", $T, sample_canon_64, g, inputs);
+        bench_distr_int_group!("Canon-Lemire", $T, sample_canon_lemire, g, inputs);
+        bench_distr_int_group!("Bitmask", $T, sample_bitmask, g, inputs);
         drop(g);
 
-        let mut g = $c.benchmark_group(concat!("uniform_int_", stringify!($T)));
-        bench_int_group!("Old", $T, sample_single_inclusive, g, inputs);
-        bench_int_group!("ONeill", $T, sample_single_inclusive_oneill, g, inputs);
-        bench_int_group!("Canon", $T, sample_single_inclusive_canon, g, inputs);
-        bench_int_group!("Canon64", $T, sample_single_inclusive_canon_64, g, inputs);
-        bench_int_group!("Canon-Lemire", $T, sample_inclusive_canon_lemire, g, inputs);
-        bench_int_group!("Bitmask", $T, sample_single_inclusive_bitmask, g, inputs);
+        let mut g = $c.benchmark_group(concat!("uniform_single_int_", stringify!($T)));
+        bench_single_int_group!("Old", $T, sample_single_inclusive, g, inputs);
+        bench_single_int_group!("ONeill", $T, sample_single_inclusive_oneill, g, inputs);
+        bench_single_int_group!("Canon", $T, sample_single_inclusive_canon, g, inputs);
+        bench_single_int_group!("Canon64", $T, sample_single_inclusive_canon_64, g, inputs);
+        bench_single_int_group!("Canon-Lemire", $T, sample_inclusive_canon_lemire, g, inputs);
+        bench_single_int_group!("Bitmask", $T, sample_single_inclusive_bitmask, g, inputs);
     }};
 }
 
