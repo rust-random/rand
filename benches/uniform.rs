6bcf5ef5a92ceb78498b190b96a4e2aa6a39df5b
Diggory Hardy <git@dhardy.name>
2022-03-01 17:54:18 +0000

    Bench: revise single-random benchmark

--- benches/uniform.rs
+++ benches/uniform.rs
@@ -31,45 +31,55 @@ const WARM_UP_TIME: Duration = Duration::from_millis(100);
 const MEASUREMENT_TIME: Duration = Duration::from_secs(1);
 
 macro_rules! single_random {
-    ($name:literal, $R:ty, $T:ty, $f:ident, $g:expr) => {
+    ($name:literal, $R:ty, $T:ty, $U:ty, $f:ident, $g:expr) => {
         $g.bench_function(BenchmarkId::new(stringify!($R), $name), |b| {
-            let low = <$T>::MIN;
             let mut rng = <$R>::from_entropy();
+            let x = rng.gen::<$U>();
+            let bits = (<$T>::BITS / 2);
+            let mask = (1 as $U).wrapping_neg() >> bits;
+            let range = (x >> bits) * (x & mask);
+            let low = <$T>::MIN;
+            let high = low.wrapping_add(range as $T);
+            const SAMPLES: usize = 50;
             b.iter(|| {
-                let high: $T = rng.gen();
-                <$T as SampleUniform>::Sampler::$f(low, high, &mut rng)
-            })
+                let mut result: $T = 0;
+                for _ in 0..SAMPLES {
+                    result =
+                        result.wrapping_add(<$T as SampleUniform>::Sampler::$f(low, high, &mut rng));
+                }
+                result
+            });
         });
     };
 
-    ($R:ty, $T:ty, $g:expr) => {
-        single_random!("Old", $R, $T, sample_single_inclusive, $g);
-        single_random!("ONeill", $R, $T, sample_single_inclusive_oneill, $g);
-        single_random!("Canon-Unbiased", $R, $T, sample_single_inclusive_canon_unbiased, $g);
-        single_random!("Canon", $R, $T, sample_single_inclusive_canon, $g);
-        single_random!("Canon32", $R, $T, sample_single_inclusive_canon_u32, $g);
-        single_random!("Canon-reduced", $R, $T, sample_single_inclusive_canon_reduced, $g);
-        single_random!("Canon-Lemire", $R, $T, sample_inclusive_canon_lemire, $g);
-        single_random!("Bitmask", $R, $T, sample_single_inclusive_bitmask, $g);
+    ($R:ty, $T:ty, $U:ty, $g:expr) => {
+        single_random!("Old", $R, $T, $U, sample_single_inclusive, $g);
+        single_random!("ONeill", $R, $T, $U, sample_single_inclusive_oneill, $g);
+        single_random!("Canon-Unbiased", $R, $T, $U, sample_single_inclusive_canon_unbiased, $g);
+        single_random!("Canon", $R, $T, $U, sample_single_inclusive_canon, $g);
+        single_random!("Canon32", $R, $T, $U, sample_single_inclusive_canon_u32, $g);
+        single_random!("Canon-reduced", $R, $T, $U, sample_single_inclusive_canon_reduced, $g);
+        single_random!("Canon-Lemire", $R, $T, $U, sample_inclusive_canon_lemire, $g);
+        single_random!("Bitmask", $R, $T, $U, sample_single_inclusive_bitmask, $g);
     };
 
-    ($c:expr, $T:ty) => {{
+    ($c:expr, $T:ty, $U:ty) => {{
         let mut g = $c.benchmark_group(concat!("single_random_", stringify!($T)));
         g.warm_up_time(WARM_UP_TIME);
         g.measurement_time(MEASUREMENT_TIME);
-        single_random!(SmallRng, $T, g);
-        single_random!(ChaCha8Rng, $T, g);
-        single_random!(Pcg32, $T, g);
-        single_random!(Pcg64, $T, g);
+        single_random!(SmallRng, $T, $U, g);
+        single_random!(ChaCha8Rng, $T, $U, g);
+        single_random!(Pcg32, $T, $U, g);
+        single_random!(Pcg64, $T, $U, g);
     }};
 }
 
 fn single_random(c: &mut Criterion) {
-    single_random!(c, i8);
-    single_random!(c, i16);
-    single_random!(c, i32);
-    single_random!(c, i64);
-    single_random!(c, i128);
+    single_random!(c, i8, u8);
+    single_random!(c, i16, u16);
+    single_random!(c, i32, u32);
+    single_random!(c, i64, u64);
+    single_random!(c, i128, u128);
 }
 
 macro_rules! distr_range {
