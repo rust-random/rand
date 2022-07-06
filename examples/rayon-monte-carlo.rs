36b7be28ac0a9db6fb39d18da37767a1cb18c9dd
Mason Kramer <mason@masonkramer.net>
2022-07-06 11:02:13 -0400

    reword the discussion

--- examples/rayon-monte-carlo.rs
+++ examples/rayon-monte-carlo.rs
@@ -33,10 +33,10 @@
 //! map_init to construct one RNG per Rayon batch.
 //!
 //! Instead, we do our own batching, so that a Rayon work item becomes a
-//! batch. Then we can fix our rng stream to the batched work item. This
-//! Then we amortizes the cost of constructing the Rng over BATCH_SIZE trials.
-//! Manually batching also turns out to be faster for the nondeterministic
-//! version of this program as well.
+//! batch. Then we can fix our rng stream to the batched work item.
+//! Batching amortizes the cost of constructing the Rng from a fixed seed
+//! over BATCH_SIZE trials. Manually batching also turns out to be faster
+//! for the nondeterministic version of this program as well.
 
 #![cfg(all(feature = "std", feature = "std_rng"))]
 
