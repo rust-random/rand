2802d25031e5572c9dbf82fa64b1846412b381c1
cactter <yeahs2022@163.com>
2022-07-22 07:49:15 +0800

    Shrink unsafe block

--- src/distributions/other.rs
+++ src/distributions/other.rs
@@ -127,10 +127,10 @@ impl Distribution<u8> for Alphanumeric {
 #[cfg(feature = "alloc")]
 impl DistString for Alphanumeric {
     fn append_string<R: Rng + ?Sized>(&self, rng: &mut R, string: &mut String, len: usize) {
-        unsafe {
-            let v = string.as_mut_vec();
+        
+            let v = unsafe { string.as_mut_vec() };
             v.extend(self.sample_iter(rng).take(len));
-        }
+        
     }
 }
 
