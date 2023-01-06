06820c2396696d610b7274c525246cc8ce53d14e
Mark Wainwright <wainwrightml@gmail.com>
2023-01-06 15:58:59 +0000

    Added and updated copyright notices

--- src/seq/mod.rs
+++ src/seq/mod.rs
@@ -1,4 +1,4 @@
-// Copyright 2018 Developers of the Rand project.
+// Copyright 2018-2023 Developers of the Rand project.
 //
 // Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
 // https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
