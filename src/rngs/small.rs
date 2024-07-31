5410c5d5760e15ceceacfff4f1c9f7d9fc4fd4f3
Arthur Silva <arthurprs@gmail.com>
2024-07-31 14:55:44 +0200

    Improve SmallRng initialization performance

--- src/rngs/small.rs
+++ src/rngs/small.rs
@@ -83,7 +83,8 @@ impl SeedableRng for SmallRng {
 
     #[inline(always)]
     fn from_seed(seed: Self::Seed) -> Self {
-        // With MSRV >= 1.77: let seed = *seed.first_chunk().unwrap();
+        // This is for compatibility with 32-bit platforms where Rng::Seed has a different seed size
+        // With MSRV >= 1.77: let seed = *seed.first_chunk().unwrap()
         const LEN: usize = core::mem::size_of::<<Rng as SeedableRng>::Seed>();
         let seed = (&seed[..LEN]).try_into().unwrap();
         SmallRng(Rng::from_seed(seed))
