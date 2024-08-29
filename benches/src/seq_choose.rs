1c156167e2d127b39b2eb07655bc7a42442a7d80
Diggory Hardy <git@dhardy.name>
2024-08-29 09:52:46 +0100

    Migrate seq_[slice|iter]_choose_multiple_* to Criterion

--- benches/src/seq_choose.rs
+++ benches/src/seq_choose.rs
@@ -28,6 +28,47 @@ pub fn bench(c: &mut Criterion) {
         b.iter(|| x.choose(&mut rng).unwrap());
     });
 
+    let lens = [(1, 1000), (950, 1000), (10, 100), (90, 100)];
+    for (amount, len) in lens {
+        let name = format!("seq_slice_choose_multiple_{}_of_{}", amount, len);
+        c.bench_function(name.as_str(), |b| {
+            let mut rng = Pcg32::from_rng(thread_rng());
+            let mut buf = [0i32; 1000];
+            rng.fill(&mut buf);
+            let x = black_box(&buf[..len]);
+
+            let mut results_buf = [0i32; 950];
+            let y = black_box(&mut results_buf[..amount]);
+            let amount = black_box(amount);
+
+            b.iter(|| {
+                // Collect full result to prevent unwanted shortcuts getting
+                // first element (in case sample_indices returns an iterator).
+                for (slot, sample) in y.iter_mut().zip(x.choose_multiple(&mut rng, amount)) {
+                    *slot = *sample;
+                }
+                y[amount - 1]
+            })
+        });
+    }
+
+    c.bench_function("seq_iter_choose_multiple_10_of_100", |b| {
+        let mut rng = Pcg32::from_rng(thread_rng());
+        let mut buf = [0i32; 100];
+        rng.fill(&mut buf);
+        let x = black_box(&buf);
+        b.iter(|| x.iter().cloned().choose_multiple(&mut rng, 10))
+    });
+
+    c.bench_function("seq_iter_choose_multiple_fill_10_of_100", |b| {
+        let mut rng = Pcg32::from_rng(thread_rng());
+        let mut buf = [0i32; 100];
+        rng.fill(&mut buf);
+        let x = black_box(&buf);
+        let mut buf = [0; 10];
+        b.iter(|| x.iter().cloned().choose_multiple_fill(&mut rng, &mut buf))
+    });
+
     bench_rng::<rand_chacha::ChaCha20Rng>(c, "ChaCha20");
     bench_rng::<rand_pcg::Pcg32>(c, "Pcg32");
     bench_rng::<rand_pcg::Pcg64>(c, "Pcg64");
