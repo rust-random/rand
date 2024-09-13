afb619d67ce111896fe93833be986cbfcee855c1
Diggory Hardy <git@dhardy.name>
2024-09-13 08:53:41 +0100

    benches/distr: use g.finish()

--- benches/benches/distr.rs
+++ benches/benches/distr.rs
@@ -111,14 +111,12 @@ macro_rules! sample_binomial {
 }
 
 fn bench(c: &mut Criterion<CyclesPerByte>) {
-    {
     let mut g = c.benchmark_group("exp");
     distr_float!(g, "exp", f64, Exp::new(1.23 * 4.56).unwrap());
     distr_float!(g, "exp1_specialized", f64, Exp1);
     distr_float!(g, "exp1_general", f64, Exp::new(1.).unwrap());
-    }
+    g.finish();
 
-    {
     let mut g = c.benchmark_group("normal");
     distr_float!(g, "normal", f64, Normal::new(-1.23, 4.56).unwrap());
     distr_float!(g, "standardnormal_specialized", f64, StandardNormal);
@@ -139,16 +137,14 @@ fn bench(c: &mut Criterion<CyclesPerByte>) {
             accum
         });
     });
-    }
+    g.finish();
 
-    {
     let mut g = c.benchmark_group("skew_normal");
     distr_float!(g, "shape_zero", f64, SkewNormal::new(0.0, 1.0, 0.0).unwrap());
     distr_float!(g, "shape_positive", f64, SkewNormal::new(0.0, 1.0, 100.0).unwrap());
     distr_float!(g, "shape_negative", f64, SkewNormal::new(0.0, 1.0, -100.0).unwrap());
-    }
+    g.finish();
 
-    {
     let mut g = c.benchmark_group("gamma");
     distr_float!(g, "gamma_large_shape", f64, Gamma::new(10., 1.0).unwrap());
     distr_float!(g, "gamma_small_shape", f64, Gamma::new(0.1, 1.0).unwrap());
@@ -156,25 +152,21 @@ fn bench(c: &mut Criterion<CyclesPerByte>) {
     distr_float!(g, "beta_large_param_similar", f64, Beta::new(101., 95.).unwrap());
     distr_float!(g, "beta_large_param_different", f64, Beta::new(10., 1000.).unwrap());
     distr_float!(g, "beta_mixed_param", f64, Beta::new(0.5, 100.).unwrap());
-    }
+    g.finish();
 
-    {
     let mut g = c.benchmark_group("cauchy");
     distr_float!(g, "cauchy", f64, Cauchy::new(4.2, 6.9).unwrap());
-    }
+    g.finish();
 
-    {
     let mut g = c.benchmark_group("triangular");
     distr_float!(g, "triangular", f64, Triangular::new(0., 1., 0.9).unwrap());
-    }
+    g.finish();
 
-    {
     let mut g = c.benchmark_group("geometric");
     distr_int!(g, "geometric", u64, Geometric::new(0.5).unwrap());
     distr_int!(g, "standard_geometric", u64, StandardGeometric);
-    }
+    g.finish();
 
-    {
     let mut g = c.benchmark_group("weighted");
     distr_int!(g, "weighted_i8", usize, WeightedIndex::new([1i8, 2, 3, 4, 12, 0, 2, 1]).unwrap());
     distr_int!(g, "weighted_u32", usize, WeightedIndex::new([1u32, 2, 3, 4, 12, 0, 2, 1]).unwrap());
@@ -184,9 +176,8 @@ fn bench(c: &mut Criterion<CyclesPerByte>) {
     distr_int!(g, "weighted_alias_method_u32", usize, WeightedAliasIndex::new(vec![1u32, 2, 3, 4, 12, 0, 2, 1]).unwrap());
     distr_int!(g, "weighted_alias_method_f64", usize, WeightedAliasIndex::new(vec![1.0f64, 0.001, 1.0/3.0, 4.01, 0.0, 3.3, 22.0, 0.001]).unwrap());
     distr_int!(g, "weighted_alias_method_large_set", usize, WeightedAliasIndex::new((0..10000).rev().chain(1..10001).collect()).unwrap());
-    }
+    g.finish();
 
-    {
     let mut g = c.benchmark_group("binomial");
     sample_binomial!(g, "binomial", 20, 0.7);
     sample_binomial!(g, "binomial_small", 1_000_000, 1e-30);
@@ -195,33 +186,28 @@ fn bench(c: &mut Criterion<CyclesPerByte>) {
     sample_binomial!(g, "binomial_100", 100, 0.99);
     sample_binomial!(g, "binomial_1000", 1000, 0.01);
     sample_binomial!(g, "binomial_1e12", 1_000_000_000_000, 0.2);
-    }
+    g.finish();
 
-    {
     let mut g = c.benchmark_group("poisson");
     distr_float!(g, "poisson", f64, Poisson::new(4.0).unwrap());
-    }
+    g.finish();
 
-    {
     let mut g = c.benchmark_group("zipf");
     distr_float!(g, "zipf", f64, Zipf::new(10, 1.5).unwrap());
     distr_float!(g, "zeta", f64, Zeta::new(1.5).unwrap());
-    }
+    g.finish();
 
-    {
     let mut g = c.benchmark_group("bernoulli");
     distr!(g, "bernoulli", bool, Bernoulli::new(0.18).unwrap());
-    }
+    g.finish();
 
-    {
     let mut g = c.benchmark_group("circle");
     distr_arr!(g, "circle", [f64; 2], UnitCircle);
-    }
+    g.finish();
 
-    {
     let mut g = c.benchmark_group("sphere");
     distr_arr!(g, "sphere", [f64; 3], UnitSphere);
-    }
+    g.finish();
 }
 
 criterion_group!(
