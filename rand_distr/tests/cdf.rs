a8e3596812cfc59ecdb73cebdda7294c59854e12
JamboChen <chenjambo@gmail.com>
2024-10-11 22:38:53 +0800

    add: ks test for `SkewNormal`

--- rand_distr/tests/cdf.rs
+++ rand_distr/tests/cdf.rs
@@ -159,6 +159,21 @@ fn normal() {
 }
 
 #[test]
+fn skew_normal() {
+    fn cdf(x: f64, location: f64, scale: f64, shape: f64) -> f64 {
+        let norm = (x - location) / scale;
+        phi(norm) - 2.0 * owen_t(norm, shape)
+    }
+
+    let parameters = [(0.0, 1.0, 5.0), (1.0, 10.0, -5.0), (-1.0, 0.00001, 0.0)];
+
+    for (seed, (location, scale, shape)) in parameters.into_iter().enumerate() {
+        let dist = rand_distr::SkewNormal::new(location, scale, shape).unwrap();
+        test_continuous(seed as u64, dist, |x| cdf(x, location, scale, shape));
+    }
+}
+
+#[test]
 fn cauchy() {
     fn cdf(x: f64, median: f64, scale: f64) -> f64 {
         (1.0 / f64::consts::PI) * ((x - median) / scale).atan() + 0.5
@@ -745,4 +760,245 @@ fn update_akn(akn: &mut Vec<f64>, s: f64) {
     let p2 = (n1 / (n1 + 1.0)).powf(s);
 
     akn.push(p1 * p2 * akn[n]);
+}
+
+
+/// [1] FAST AND ACCURATE CALCULATION OF OWEN’S T-FUNCTION
+///     by Mike Patefield and David Tandy
+///     http://www.jstatsoft.org/v05/i05/paper
+
+fn owen_t(h: f64, a: f64) -> f64 {
+    let absh = h.abs();
+    let absa = a.abs();
+    let ah = absa * absh;
+
+    let mut t;
+    if absa <= 1.0 {
+        t = tf(absh, absa, ah);
+    } else if absh <= 0.67 {
+        t = 0.25 - znorm1(absh) * znorm1(ah) - tf(ah, 1.0 / absa, absh);
+    } else {
+        let normh = znorm2(absh);
+        let normah = znorm2(ah);
+        t = 0.5 * (normh + normah) - normh * normah - tf(ah, 1.0 / absa, absh);
+    }
+
+    if a < 0.0 {
+        t = -t;
+    }
+
+    t
+}
+
+fn tf(h: f64, a: f64, ah: f64) -> f64 {
+    let rtwopi = 0.15915494309189533577;
+    let rrtpi = 0.39894228040143267794;
+
+    let c2 = [
+        0.99999999999999987510,
+        -0.99999999999988796462,
+        0.99999999998290743652,
+        -0.99999999896282500134,
+        0.99999996660459362918,
+        -0.99999933986272476760,
+        0.99999125611136965852,
+        -0.99991777624463387686,
+        0.99942835555870132569,
+        -0.99697311720723000295,
+        0.98751448037275303682,
+        -0.95915857980572882813,
+        0.89246305511006708555,
+        -0.76893425990463999675,
+        0.58893528468484693250,
+        -0.38380345160440256652,
+        0.20317601701045299653,
+        -0.82813631607004984866e-01,
+        0.24167984735759576523e-01,
+        -0.44676566663971825242e-02,
+        0.39141169402373836468e-03,
+    ];
+
+    let pts = [
+        0.35082039676451715489e-02,
+        0.31279042338030753740e-01,
+        0.85266826283219451090e-01,
+        0.16245071730812277011,
+        0.25851196049125434828,
+        0.36807553840697533536,
+        0.48501092905604697475,
+        0.60277514152618576821,
+        0.71477884217753226516,
+        0.81475510988760098605,
+        0.89711029755948965867,
+        0.95723808085944261843,
+        0.99178832974629703586,
+    ];
+
+    let wts = [
+        0.18831438115323502887e-01,
+        0.18567086243977649478e-01,
+        0.18042093461223385584e-01,
+        0.17263829606398753364e-01,
+        0.16243219975989856730e-01,
+        0.14994592034116704829e-01,
+        0.13535474469662088392e-01,
+        0.11886351605820165233e-01,
+        0.10070377242777431897e-01,
+        0.81130545742299586629e-02,
+        0.60419009528470238773e-02,
+        0.38862217010742057883e-02,
+        0.16793031084546090448e-02,
+    ];
+
+    let hrange = [
+        0.02, 0.06, 0.09, 0.125, 0.26, 0.4, 0.6, 1.6, 1.7, 2.33, 2.4, 3.36, 3.4, 4.8,
+    ];
+    let arange = [0.025, 0.09, 0.15, 0.36, 0.5, 0.9, 0.99999];
+
+    let select = [
+        [1, 1, 2, 13, 13, 13, 13, 13, 13, 13, 13, 16, 16, 16, 9],
+        [1, 2, 2, 3, 3, 5, 5, 14, 14, 15, 15, 16, 16, 16, 9],
+        [2, 2, 3, 3, 3, 5, 5, 15, 15, 15, 15, 16, 16, 16, 10],
+        [2, 2, 3, 5, 5, 5, 5, 7, 7, 16, 16, 16, 16, 16, 10],
+        [2, 3, 3, 5, 5, 6, 6, 8, 8, 17, 17, 17, 12, 12, 11],
+        [2, 3, 5, 5, 5, 6, 6, 8, 8, 17, 17, 17, 12, 12, 12],
+        [2, 3, 4, 4, 6, 6, 8, 8, 17, 17, 17, 17, 17, 12, 12],
+        [2, 3, 4, 4, 6, 6, 18, 18, 18, 18, 17, 17, 17, 12, 12],
+    ];
+
+    let ihint = match hrange.iter().position(|&r| h < r) {
+        Some(idx) => idx,
+        None => 14,
+    };
+
+    let iaint = match arange.iter().position(|&r| a < r) {
+        Some(idx) => idx,
+        None => 7,
+    };
+
+    let icode = select[iaint][ihint];
+    let m = [
+        2, 3, 4, 5, 7, 10, 12, 18, 10, 20, 30, 20, 4, 7, 8, 20, 13, 0,
+    ][icode - 1];
+    let method = [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 4, 4, 4, 4, 5, 6][icode - 1];
+
+    match method {
+        1 => {
+            let hs = -0.5 * h * h;
+            let dhs = hs.exp();
+            let as_ = a * a;
+            let mut j = 1;
+            let mut jj = 1;
+            let mut aj = rtwopi * a;
+            let mut tf = rtwopi * a.atan();
+            let mut dj = dhs - 1.0;
+            let mut gj = hs * dhs;
+            loop {
+                tf += dj * aj / (jj as f64);
+                if j >= m {
+                    return tf;
+                }
+                j += 1;
+                jj += 2;
+                aj *= as_;
+                dj = gj - dj;
+                gj *= hs / (j as f64);
+            }
+        }
+        2 => {
+            let maxii = m + m + 1;
+            let mut ii = 1;
+            let mut tf = 0.0;
+            let hs = h * h;
+            let as_ = -a * a;
+            let mut vi = rrtpi * a * (-0.5 * ah * ah).exp();
+            let mut z = znorm1(ah) / h;
+            let y = 1.0 / hs;
+            loop {
+                tf += z;
+                if ii >= maxii {
+                    tf *= rrtpi * (-0.5 * hs).exp();
+                    return tf;
+                }
+                z = y * (vi - (ii as f64) * z);
+                vi *= as_;
+                ii += 2;
+            }
+        }
+        3 => {
+            let mut i = 1;
+            let mut ii = 1;
+            let mut tf = 0.0;
+            let hs = h * h;
+            let as_ = a * a;
+            let mut vi = rrtpi * a * (-0.5 * ah * ah).exp();
+            let mut zi = znorm1(ah) / h;
+            let y = 1.0 / hs;
+            loop {
+                tf += zi * c2[i - 1];
+                if i > m {
+                    tf *= rrtpi * (-0.5 * hs).exp();
+                    return tf;
+                }
+                zi = y * ((ii as f64) * zi - vi);
+                vi *= as_;
+                i += 1;
+                ii += 2;
+            }
+        }
+        4 => {
+            let maxii = m + m + 1;
+            let mut ii = 1;
+            let mut tf = 0.0;
+            let hs = h * h;
+            let as_ = -a * a;
+            let mut ai = rtwopi * a * (-0.5 * hs * (1.0 - as_)).exp();
+            let mut yi = 1.0;
+            loop {
+                tf += ai * yi;
+                if ii >= maxii {
+                    return tf;
+                }
+                ii += 2;
+                yi = (1.0 - hs * yi) / (ii as f64);
+                ai *= as_;
+            }
+        }
+        5 => {
+            let mut tf = 0.0;
+            let as_ = a * a;
+            let hs = -0.5 * h * h;
+            for i in 0..m {
+                let r = 1.0 + as_ * pts[i];
+                tf += wts[i] * (hs * r).exp() / r;
+            }
+            tf *= a;
+            return tf;
+        }
+        6 => {
+            let normh = znorm2(h);
+            let mut tf = 0.5 * normh * (1.0 - normh);
+            let y = 1.0 - a;
+            let r = (y / (1.0 + a)).atan();
+            if r != 0.0 {
+                tf -= rtwopi * r * (-0.5 * y * h * h / r).exp();
+            }
+            return tf;
+        }
+        _ => 0.0,
+    }
+}
+
+fn phi(x: f64) -> f64 {
+    normal_cdf(x, 0.0, 1.0)
+}
+
+// P(0 ≤ Z ≤ x)
+fn znorm1(x: f64) -> f64 {
+    phi(x) - 0.5
+}
+
+// P(x ≤ Z < ∞)
+fn znorm2(x: f64) -> f64 {
+    1.0 - phi(x)
 }
