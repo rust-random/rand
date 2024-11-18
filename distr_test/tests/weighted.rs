eb1836bb343627d189371331115b6478f5f18feb
Diggory Hardy <git@dhardy.name>
2024-11-18 17:05:51 +0000

    Add KS test for WeightedAliasIndex

--- distr_test/tests/weighted.rs
+++ distr_test/tests/weighted.rs
@@ -9,24 +9,50 @@
 mod ks;
 use ks::test_discrete;
 use rand::distr::WeightedIndex;
+use rand_distr::WeightedAliasIndex;
+
+fn make_cdf(num: usize, f: impl Fn(i64) -> f64) -> impl Fn(i64) -> f64 {
+    let mut cdf = Vec::with_capacity(num);
+    let mut ac = 0.0;
+    for i in 0..num {
+        ac += f(i as i64);
+        cdf.push(ac);
+    }
+
+    let frac = 1.0 / ac;
+    for x in &mut cdf {
+        *x = *x * frac;
+    }
+
+    move |i| {
+        if i < 0 {
+            0.0
+        } else {
+            cdf[i as usize]
+        }
+    }
+}
 
 #[test]
 fn weighted_index() {
     fn test_weights(num: usize, weight: impl Fn(i64) -> f64) {
         let distr = WeightedIndex::new((0..num).map(|i| weight(i as i64))).unwrap();
+        test_discrete(0, distr, make_cdf(num, weight));
+    }
 
-        let mut cdf = Vec::with_capacity(num);
-        let mut ac = 0.0;
-        for i in 0..num {
-            ac += weight(i as i64);
-            cdf.push(ac);
-        }
-        let frac = 1.0 / ac;
-        for x in &mut cdf {
-            *x = *x * frac;
-        }
+    test_weights(100, |_| 1.0);
+    test_weights(100, |i| ((i + 1) as f64).ln());
+    test_weights(100, |i| i as f64);
+    test_weights(100, |i| (i as f64).powi(3));
+    test_weights(100, |i| 1.0 / ((i + 1) as f64));
+}
 
-        test_discrete(0, distr, |i| if i < 0 { 0.0 } else { cdf[i as usize] });
+#[test]
+fn weighted_alias_index() {
+    fn test_weights(num: usize, weight: impl Fn(i64) -> f64) {
+        let weights = (0..num).map(|i| weight(i as i64)).collect();
+        let distr = WeightedAliasIndex::new(weights).unwrap();
+        test_discrete(0, distr, make_cdf(num, weight));
     }
 
     test_weights(100, |_| 1.0);
