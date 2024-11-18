a8ce256881a2733ee36162de9e8a3e4417bf3be3
Diggory Hardy <git@dhardy.name>
2024-11-18 17:06:58 +0000

    Add KS test for WeightedTreeIndex

--- distr_test/tests/weighted.rs
+++ distr_test/tests/weighted.rs
@@ -9,7 +9,7 @@
 mod ks;
 use ks::test_discrete;
 use rand::distr::WeightedIndex;
-use rand_distr::WeightedAliasIndex;
+use rand_distr::{WeightedAliasIndex, WeightedTreeIndex};
 
 fn make_cdf(num: usize, f: impl Fn(i64) -> f64) -> impl Fn(i64) -> f64 {
     let mut cdf = Vec::with_capacity(num);
@@ -52,6 +52,20 @@ fn weighted_alias_index() {
     fn test_weights(num: usize, weight: impl Fn(i64) -> f64) {
         let weights = (0..num).map(|i| weight(i as i64)).collect();
         let distr = WeightedAliasIndex::new(weights).unwrap();
+        test_discrete(0, distr, make_cdf(num, weight));
+    }
+
+    test_weights(100, |_| 1.0);
+    test_weights(100, |i| ((i + 1) as f64).ln());
+    test_weights(100, |i| i as f64);
+    test_weights(100, |i| (i as f64).powi(3));
+    test_weights(100, |i| 1.0 / ((i + 1) as f64));
+}
+
+#[test]
+fn weighted_tree_index() {
+    fn test_weights(num: usize, weight: impl Fn(i64) -> f64) {
+        let distr = WeightedTreeIndex::new((0..num).map(|i| weight(i as i64))).unwrap();
         test_discrete(0, distr, make_cdf(num, weight));
     }
 
