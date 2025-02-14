be743d53d3404c9b13486c266b3b982fdd6c38ed
1Kill2Steal <171241044+1Git2Clone@users.noreply.github.com>
2025-02-14 18:13:22 +0200

    Map `u8` iterator with `char::from` instead of working with `String.inner`

--- src/distr/other.rs
+++ src/distr/other.rs
@@ -182,10 +182,7 @@ impl SampleString for Alphanumeric {
 #[cfg(feature = "alloc")]
 impl SampleString for Alphabetic {
     fn append_string<R: Rng + ?Sized>(&self, rng: &mut R, string: &mut String, len: usize) {
-        unsafe {
-            let v = string.as_mut_vec();
-            v.extend(self.sample_iter(rng).take(len));
-        }
+        string.extend(self.sample_iter(rng).take(len).map(char::from));
     }
 }
 
