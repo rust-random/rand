9e8c0411364dd99833d415f7eba44772938af876
1Kill2Steal <171241044+1Git2Clone@users.noreply.github.com>
2025-02-14 17:06:48 +0200

    Implement `SampleString` for `Alphabetic`

--- src/distr/other.rs
+++ src/distr/other.rs
@@ -147,6 +147,16 @@ impl SampleString for Alphanumeric {
     }
 }
 
+#[cfg(feature = "alloc")]
+impl SampleString for Alphabetic {
+    fn append_string<R: Rng + ?Sized>(&self, rng: &mut R, string: &mut String, len: usize) {
+        unsafe {
+            let v = string.as_mut_vec();
+            v.extend(self.sample_iter(rng).take(len));
+        }
+    }
+}
+
 impl Distribution<bool> for StandardUniform {
     #[inline]
     fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> bool {
