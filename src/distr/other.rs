c9093cbf5bfdaf377ccd10148ac304dc46c08225
1Kill2Steal <171241044+1Git2Clone@users.noreply.github.com>
2025-02-15 12:42:45 +0200

    Reimplemented `String::extend` with `&mut Vec<u8>`

--- src/distr/other.rs
+++ src/distr/other.rs
@@ -176,7 +176,14 @@ impl SampleString for Alphanumeric {
 #[cfg(feature = "alloc")]
 impl SampleString for Alphabetic {
     fn append_string<R: Rng + ?Sized>(&self, rng: &mut R, string: &mut String, len: usize) {
-        string.extend(self.sample_iter(rng).take(len).map(char::from));
+        // SAFETY: With this distribution we guarantee that we're working with valid ASCII
+        // characters.
+        // See [#1590](https://github.com/rust-random/rand/issues/1590).
+        unsafe {
+            let v = string.as_mut_vec();
+            v.reserve_exact(len);
+            v.extend(self.sample_iter(rng).take(len));
+        }
     }
 }
 
