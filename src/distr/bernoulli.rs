311a6117acc26714c3705e072d4c598c43101967
AbeZbm <128758303@qq.com>
2025-05-24 22:04:44 +0800

    Add some tests for BlockRng, BlockRng64, Bernoulli and Xoshiro256PlusPlus

--- src/distr/bernoulli.rs
+++ src/distr/bernoulli.rs
@@ -243,7 +243,6 @@ mod test {
 
     #[test]
     fn bernoulli_p() {
-        use super::SCALE;
         assert_eq!(Bernoulli::new(0.0).unwrap().p(), 0.0);
         assert_eq!(Bernoulli::new(0.5).unwrap().p(), 0.5);
         assert_eq!(Bernoulli::new(1.0).unwrap().p(), 1.0);
