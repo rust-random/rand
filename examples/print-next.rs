cd47b5f84dd15b69e177a7269bad97a125938fae
Diggory Hardy <git@dhardy.name>
2025-07-16 13:16:38 +0100

    Add examples/print-next.rs

---
+++ examples/print-next.rs
@@ -0,0 +1,7 @@
+use rand::RngCore;
+
+fn main() {
+    let mut rng = rand::rng();
+    println!("Next u32: {0:>#18X} = {0:>20}", rng.next_u32());
+    println!("Next u64: {0:>#18X} = {0:>20}", rng.next_u64());
+}
