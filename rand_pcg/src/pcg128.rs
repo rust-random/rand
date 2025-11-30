25402ef21d9d5c36e5c543bfb50eb2a80b9a2a7a
Diggory Hardy <git@dhardy.name>
2025-11-30 11:10:16 +0000

    Update rand_pcg

--- rand_pcg/src/pcg128.rs
+++ rand_pcg/src/pcg128.rs
@@ -14,7 +14,7 @@
 const MULTIPLIER: u128 = 0x2360_ED05_1FC6_5DA4_4385_DF64_9FCC_F645;
 
 use core::fmt;
-use rand_core::{RngCore, SeedableRng, le};
+use rand_core::{RngCore, SeedableRng, utils};
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
 
@@ -126,8 +126,7 @@ impl SeedableRng for Lcg128Xsl64 {
     /// We use a single 255-bit seed to initialise the state and select a stream.
     /// One `seed` bit (lowest bit of `seed[8]`) is ignored.
     fn from_seed(seed: Self::Seed) -> Self {
-        let mut seed_u64 = [0u64; 4];
-        le::read_u64_into(&seed, &mut seed_u64);
+        let seed_u64: [u64; 4] = utils::read_words(&seed);
         let state = u128::from(seed_u64[0]) | (u128::from(seed_u64[1]) << 64);
         let incr = u128::from(seed_u64[2]) | (u128::from(seed_u64[3]) << 64);
 
@@ -150,7 +149,7 @@ impl RngCore for Lcg128Xsl64 {
 
     #[inline]
     fn fill_bytes(&mut self, dest: &mut [u8]) {
-        le::fill_bytes_via_next(self, dest)
+        utils::fill_bytes_via_next_word(dest, || self.next_u64());
     }
 }
 
@@ -232,8 +231,7 @@ impl SeedableRng for Mcg128Xsl64 {
 
     fn from_seed(seed: Self::Seed) -> Self {
         // Read as if a little-endian u128 value:
-        let mut seed_u64 = [0u64; 2];
-        le::read_u64_into(&seed, &mut seed_u64);
+        let seed_u64: [u64; 2] = utils::read_words(&seed);
         let state = u128::from(seed_u64[0]) | (u128::from(seed_u64[1]) << 64);
         Mcg128Xsl64::new(state)
     }
@@ -253,7 +251,7 @@ impl RngCore for Mcg128Xsl64 {
 
     #[inline]
     fn fill_bytes(&mut self, dest: &mut [u8]) {
-        le::fill_bytes_via_next(self, dest)
+        utils::fill_bytes_via_next_word(dest, || self.next_u64());
     }
 }
 
