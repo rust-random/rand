f79dc484deb87ade283342507356397ef2cc61e4
Diggory Hardy <git@dhardy.name>
2026-06-18 07:42:56 +0000

    Avoid unsafe where safety depends on non-local values

--- src/distr/slice.rs
+++ src/distr/slice.rs
@@ -86,18 +86,9 @@ impl<'a, T> Choose<'a, T> {
 impl<'a, T> Distribution<&'a T> for Choose<'a, T> {
     fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> &'a T {
         let idx = self.range.sample(rng);
-
-        debug_assert!(
-            idx < self.slice.len(),
-            "Uniform::new(0, {}) somehow returned {}",
-            self.slice.len(),
-            idx
-        );
-
-        // Safety: at construction time, it was ensured that the slice was
-        // non-empty, and that the `Uniform` range produces values in range
-        // for the slice
-        unsafe { self.slice.get_unchecked(idx) }
+        self.slice
+            .get(idx)
+            .expect("rand::distr::slice::Choose: index out-of-range (likely memory corruption)")
     }
 }
 
--- src/distr/uniform_other.rs
+++ src/distr/uniform_other.rs
@@ -83,10 +83,9 @@ impl UniformSampler for UniformChar {
         if x >= CHAR_SURROGATE_START {
             x += CHAR_SURROGATE_LEN;
         }
-        // SAFETY: x must not be in surrogate range or greater than char::MAX.
-        // This relies on range constructors which accept char arguments.
-        // Validity of input char values is assumed.
-        unsafe { core::char::from_u32_unchecked(x) }
+
+        char::from_u32(x)
+            .expect("rand::distr::uniform::UniformChar: invalid Unicode scalar value (likely memory corruption)")
     }
 }
 
