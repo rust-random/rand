e67515c165ca216f13e8eb240d1059c254b28062
Diggory Hardy <git@dhardy.name>
2026-08-30 10:01:22 +0000

    fn max: support underflow for signed samplers

--- src/distr/uniform_int.rs
+++ src/distr/uniform_int.rs
@@ -82,13 +82,11 @@ macro_rules! uniform_int_impl {
                 if self.range == 0 {
                     return <$ty>::MAX;
                 } else {
-                    // The checked_add op should not overflow with a correctly
-                    // constructed sampler, but it is possible to construct a
-                    // sampler that will overflow using serde.
-                    // In this case some sample value may yield MAX.
-                    self.low
-                        .checked_add(self.range.wrapping_sub(1))
-                        .unwrap_or(<$ty>::MAX)
+                    // Wrapping through <$ty>::MIN is possible with a valid
+                    // sampler over signed types. Wrapping through <$ty>::MAX is
+                    // possible with a bad sampler (constructible using serde).
+                    let max = self.low.wrapping_add(self.range.wrapping_sub(1));
+                    if max < self.low { <$ty>::MAX } else { max }
                 }
             }
         }
